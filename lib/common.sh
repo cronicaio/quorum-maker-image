4326d0cfa76e3a9f8e9874c14a85470566887947
Alexey Koshlatyy <32518348+alexeykoshlatyy@users.noreply.github.com>
2018-09-17 13:32:07 +0300

    Deleted interactivity
    Deleted docker interactivity during the building process

--- lib/common.sh
+++ lib/common.sh
@@ -4,5 +4,5 @@ CYAN=$'\e[1;96m'
 COLOR_END=$'\033[0m' # No Color
 
 function chownDir {
-    docker run -it --rm -v $(pwd)/$1:/$1 -w / -e QUID=$UID -e QGID=$(id -g $USER) golang:1.10.2 /bin/sh -c 'chown -R $QUID:$QGID '$1
+    docker run --rm -v $(pwd)/$1:/$1 -w / -e QUID=$UID -e QGID=$(id -g $USER) golang:1.10.2 /bin/sh -c 'chown -R $QUID:$QGID '$1
 }
